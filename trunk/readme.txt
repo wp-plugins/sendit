3248c646a98b26068e7f89cc5729e7cce8c8e8dd
bulini <bulini@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-16 22:27:26 +0000

    new release with polish language
    git-svn-id: https://plugins.svn.wordpress.org/sendit/tags/1.5.1@253345 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/readme.txt
@@ -0,0 +1,38 @@
+=== Sendit ===
+Contributors: Giuseppe Surace
+Donate link: http://www.giuseppesurace.com/wordpress/
+Tags: newsletter, mailing list, mailinglist, subscription form
+Requires at least: 2.3.0
+Tested up to: 3.0
+Stable tag: 1.5.1
+
+Sendit is a mailing lists and newsletter management system.
+
+== Description == 
+New version fixes historical bugs like gmail smtp settings and confirmation message 
+Sendit enable you to send newsletters and collect subscribers from your blog. You can manage one or more list and using in post/pages or using the widget(customizable). You can extract content from post or pages or create your content to send to subscribers. You can also edit the template for each mailing lists just put some XHTML code on header and footer. Newsletter will be send in XHTML format.
+
+All other information (general, changelog, installation, upgrade, usage) you need about this plugin can be found here: [Sendit readme](http://www.giuseppesurace.com/ "Sendit Readme").
+It is the exact same readme.html is included in the zip package.
+
+== Development Blog ==
+
+[Giuseppe Surace WP development blog](http://www.giuseppesurace.com "Italian Wp designer and plugin developer")
+
+== Installation ==
+
+1. Upload `sendit dir` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Go to settings and edit your mailing list or create new one
+4. Setup (if needed) your SMTP favorite settings
+5. Setup (if you have large list) the interval and how many emails to sends
+
+[Sendit Readme](http://www.giuseppesurace.com "Sendit Readme") (Installation Tab)
+
+== Screenshots ==
+
+[Sendit Screenshots](http://www.giuseppesurace.com/ "Sendit Screenshots")
+
+== Frequently Asked Questions ==
+
+[Sendit Support Forums](http://www.giuseppesurace.com/plugin-lab/ "Sendit Support Forums")
---
+++ trunk/submit.php
@@ -0,0 +1,131 @@
+<?php include("../../../wp-blog-header.php");
+
+
+/**AZIONE DI INSERIMENTO DALLA FORM DEL SITO**/
+
+function AggiungiEmail() {
+    global $_POST;
+    global $wpdb;
+    require_once('class.phpmailer.php');
+    include_once('class.smtp.php');
+    
+    $table_email = $wpdb->prefix . "nl_email";
+    
+    //messaggio di successo
+     $successo="'<div id=\"message\" class=\"updated fade\"><p><strong>".__('Subscription completed now Check your email and confirm', 'sendit')."</p></div>'";
+    //messaggio di errore
+    $errore="'<div id=\"message\" class=\"updated fade\"><p><strong>".__('not valid email address', 'sendit')."</strong></p></div>'";
+    
+    if(isset($_POST['email_add'])):   
+    
+    if (!ereg("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$", $_POST['email_add'])) :
+       
+               die( "document.getElementById('dati').innerHTML = $errore;" );
+
+      else :
+
+        
+        $user_count = $wpdb->get_var("SELECT COUNT(*) FROM $table_email where email ='$_POST[email_add]' and id_lista = '$_POST[lista]';");
+        
+            if($user_count>0) :
+                $errore_presente = "'<div class=\"error\">".__('email address already present', 'sendit')."</div>'";
+                die( "document.getElementById('dati').innerHTML = $errore_presente;" );
+            else :
+            
+                //genero stringa univoca x conferme sicure
+                $code = md5(uniqid(rand(), true));
+                
+                 $wpdb->query("INSERT INTO $table_email (email, id_lista, magic_string, accepted) VALUES ('$_POST[email_add]', '$_POST[lista]','$code','n')");
+                        
+                 /*qui mando email*/
+                
+                $table_liste = $wpdb->prefix . "nl_liste";
+                
+                     $templaterow=$wpdb->get_row("SELECT * from $table_liste where id_lista = '$_POST[lista]' ");
+                    //costruisco il messaggio come oggetto composto da $gheader $messagio $ footer
+                    
+                    //utile anzi fondamentale
+                    $plugindir   = "sendit/";
+                    $sendit_root = get_option('siteurl') . '/wp-content/plugins/'.$plugindir;
+                    $siteurl = get_option('siteurl');
+                    
+                    $header= $templaterow->header;
+                    //$messaggio= $templaterow->welcome;
+                    $welcome = __('Welcome to newsletter by: ', 'sendit').get_bloginfo('blog_name');
+                    $messaggio= "<h3>".$welcome."</h3>";
+                    $messaggio.=__('To confirm your subscription please follow this link', 'sendit').":<br />
+                    <a href=\"".$sendit_root."confirmation.php?action=confirm&c=".$code."\">".__('Confirm here', 'sendit')."</a>";
+                    $footer= $templaterow->footer;
+                    
+                    $content_send = $header.$messaggio.$footer;
+                    #### Creo object PHPMailer e imposto le COSTANTI SMTP PHPMAILER
+                    $mail = new PHPMailer();
+
+                    if(get_option('sendit_smtp_host')!='') :    
+                    //print_r($mail);
+                        $mail->IsSMTP(); // telling the class to use SMTP
+                        
+                        
+                        $mail->Host = get_option('sendit_smtp_host'); // Host
+                        $mail->Hostname = get_option('sendit_smtp_hostname');// SMTP server hostname
+                        $mail->Port  = get_option('sendit_smtp_port');// set the SMTP port
+                        
+                        if(get_option('sendit_smtp_username')!=''):    
+                            $mail->SMTPAuth = true;     // turn on SMTP authentication
+                            $mail->Username = get_option('sendit_smtp_username');  // SMTP username
+                            $mail->Password = get_option('sendit_smtp_password'); // SMTP password
+            
+								if(get_option('sendit_smtp_ssl')!=''):	
+									$mail->SMTPSecure = get_option('sendit_smtp_ssl'); // SMTP ssl
+								endif;
+
+                        else :
+                            $mail->SMTPAuth = false;// disable SMTP authentication
+                        endif;
+                    endif;
+                    
+                    $mail->SetFrom($templaterow->email_lista);
+                    //$mail->AddReplyTo('pinobulini@gmail.com');
+                    $mail->Subject = $welcome;
+                    $mail->AltBody = " To view the message, please use an HTML compatible email viewer!";
+                    // optional, comment out and test
+                    $mail->MsgHTML($content_send);
+                    
+                     $admin_mail_message = __('New subscriber for your newsletter: ', 'sendit'). get_bloginfo('blog_name');        
+
+                     $mail->AddAddress($_POST['email_add']);
+                     if($mail->Send()) :
+                        //echo $successo ;
+                         die( "document.getElementById('dati').innerHTML = $successo;" );
+                     else :
+                         
+                         //echo $errore;
+                         die( "document.getElementById('dati').innerHTML = $errore;" );
+                     endif;
+                    //notifica a admin
+                    $mail->ClearAddresses();
+                    $mail->AddAddress($templaterow->email_lista);
+                    $mail->Subject = $admin_mail_message;
+                    $mail->AltBody = __('New subscriber for your newsletter: ', 'sendit').get_bloginfo('blog_name');
+                    // optional, comment out and test
+                    $mail->MsgHTML($_POST['email_add'].__(' subscribe to your mailing list:   ').get_bloginfo('url'));
+                    $mail->Send();
+                endif;
+
+            endif;    
+    
+    endif;
+
+
+}
+
+
+
+
+
+
+AggiungiEmail();
+
+
+
+?>
