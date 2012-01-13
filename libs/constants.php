528ae5a7df80657ab0672beead64f1dde4909ba2
bulini <bulini@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-13 16:07:36 +0000

    2.0.8 fixes upgrade issues
    git-svn-id: https://plugins.svn.wordpress.org/sendit/trunk@489449 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- libs/constants.php
+++ libs/constants.php
@@ -3,8 +3,8 @@
 global $wpdb;
 define('SENDIT_EMAIL_TABLE', $wpdb->prefix . "nl_email");
 define('SENDIT_LIST_TABLE', $wpdb->prefix . "nl_liste");
-define('SENDIT_VERSION', '2.0.7');
-define('SENDIT_DB_VERSION', '2.7');
+define('SENDIT_VERSION', '2.0.8');
+define('SENDIT_DB_VERSION', '2.0.8');
 
 
 ?>
