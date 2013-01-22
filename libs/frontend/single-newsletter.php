3d7ec9dfab90e0f5bdbd30c82053dc0bac5d9beb
bulini <bulini@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-22 19:01:46 +0000

    tagging version 2.1.2
    git-svn-id: https://plugins.svn.wordpress.org/sendit/trunk@657033 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ libs/frontend/single-newsletter.php
@@ -0,0 +1,95 @@
+<?php
+/* The Newsletter template
+It will recognize automatically presence of Sendit Pro Template Manager
+and switch header footer css to template post type, if not get info (header / footer / css ) from the list_details
+*/
+
+
+
+	$sendit = new Actions();
+
+
+
+	?>
+<?php if (have_posts()) : 
+	while (have_posts()) : the_post(); 
+	$template_id= get_post_meta($post->ID, 'template_id', TRUE); 
+
+		global $post; 
+		//default free plugin value
+		$sendit_list = get_post_meta($post->ID, 'sendit_list',true);	
+		$list_detail = $sendit->GetListDetail($sendit_list);
+
+		$header=$list_detail->header;
+		$footer=$list_detail->footer;
+		$css='';
+
+
+	if(function_exists('sendit_pro_template_screen')):
+	
+
+		$template_id= get_post_meta($post->ID, 'template_id', TRUE);    		
+	$css=get_post_meta($template_id, 'newsletter_css', TRUE); 
+
+		$header=get_post_meta($template_id, 'headerhtml', TRUE);
+     	$box_styles='.info, .success, .warning, .error, .validation {
+    border: 1px solid;
+    margin: 10px 0px;
+    padding:15px 10px 15px 50px;
+    background-repeat: no-repeat;
+    background-position: 10px center;
+}
+.info {
+    color: #00529B;
+    background-color: #BDE5F8;
+}
+.success {
+    color: #4F8A10;
+    background-color: #DFF2BF;
+}
+.warning {
+    color: #9F6000;
+    background-color: #FEEFB3;
+}
+.error {
+    color: #D8000C;
+    background-color: #FFBABA;
+}';
+
+		$header=str_replace('[style]','<style>'.$css.$box_styles.'</style>',$header);
+			
+			if ( has_post_thumbnail($template_id) ) {
+				$header_image=get_the_post_thumbnail($template_id);
+				}
+			else {
+				$header_image='<img alt="" src="http://placehold.it/300x50/" />';
+			}
+			
+			$header=str_replace('[logo]',$header_image,$header);
+			$header=str_replace('[homeurl]',get_bloginfo('siteurl'),$header);
+		
+		$footer=get_post_meta($template_id, 'footerhtml', TRUE);
+
+	endif; //plugin is active
+
+		$newsletter_content=$header.get_the_content().$footer;
+
+ 		if(function_exists('inline_newsletter')):    ?>		
+     	<? endif; 
+
+		
+		//verify if inliner is installed
+		if(function_exists('inline_newsletter')):
+			$newsletter_content=inline_newsletter($css,$newsletter_content);
+		endif;
+		
+		echo $newsletter_content;
+		
+		
+		
+	endwhile;
+
+else : ?>
+     <!-- Stuff to do if there are no posts-->
+	<h2>No Newsletter</h2>
+<?php endif; ?>
