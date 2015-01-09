1594c1ecdb5466d2612f39827e8525dfceea9213
bulini <bulini@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-09 23:45:05 +0000

    fixes admin notices
    git-svn-id: https://plugins.svn.wordpress.org/sendit/trunk@1064264 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- libs/constants.php
+++ libs/constants.php
@@ -3,6 +3,6 @@
 global $wpdb;
 define('SENDIT_EMAIL_TABLE', $wpdb->prefix . "nl_email");
 define('SENDIT_LIST_TABLE', $wpdb->prefix . "nl_liste");
-define('SENDIT_VERSION', '2.3.7');
-define('SENDIT_DB_VERSION', '2.3.7');
+define('SENDIT_VERSION', '2.3.8');
+define('SENDIT_DB_VERSION', '2.3.8');
 ?>
